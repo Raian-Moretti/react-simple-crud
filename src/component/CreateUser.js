50a667cffaa1514eac54e5ff362f01b73df7e65e
Raian-Moretti <raian.moretti.eletro@gmail.com>
2021-08-18 15:32:45 -0300

    CreateUser Component

---
+++ src/component/CreateUser.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import { Context } from '../context/States';
+
+export const CreateUser = () => {
+    let userHist = useHistory();
+
+
+    const { createUser, users } = useContext(Context);
+
+    const [name, setName] = useState("");
+    const [role, setRole] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newUser = {
+            id: users.length + 1,
+            name,
+            role
+        };
+        createUser(newUser);
+        userHist.push("/");
+    };
+    return (
+        <React.Fragment>
+            <div className="A">
+                <form onSubmit={onSubmit}>
+                    <label>
+                        User
+                    </label>
+                    <input className="Input" 
+                            value={name} 
+                            onChange={(e) => setName(e.target.value)} 
+                            type="text" 
+                            placeholder="Insert Name"/>
+                    <label>
+                        Role
+                    </label>
+                    <input className="Input" 
+                            value={role} onChange={(e) => setRole(e.target.value)} 
+                            type="text" 
+                            placeholder="Insert Role"/>
+                    <div className="CreateUser">
+                        <button className="AddButton">
+                            Add User
+                        </button>
+                    </div>
+                    <div className="Cancel">
+                        <Link to="/">Cancel</Link>
+                    </div>
+                </form>
+            </div>
+        </React.Fragment>
+    )
+}
